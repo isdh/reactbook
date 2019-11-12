01ef4f720ee4aabc9663c05677738b4f5e28db4d
Tomo Yanagi <hideorseekyes+github@gmail.com>
2019-11-12 23:28:18 +0900

    add key property for array elements

--- chapter3/Excel.js
+++ chapter3/Excel.js
@@ -1,10 +1,11 @@
 var Excel = React.createClass({
+    displayName: "Excel", // displayNameプロパティを使用すると、React側に名前を伝えられる。
     render: function (){
         return (
             React.DOM.table(null,
                 React.DOM.thead(null,
                     React.DOM.tr(null,
-                        this.props.headers.map(title=>React.DOM.th(null, title))
+                        this.props.headers.map((title,index)=>React.DOM.th({key: index}, title))
                     )
                 )
             )
