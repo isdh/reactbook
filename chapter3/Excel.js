e022c67fbfce155b9c8305068e99ca772324fec7
Tomo Yanagi <isdh@users.noreply.github.com>
2019-11-14 08:37:09 +0900

    Update Excel.js
    3.4 add React.PropTypes

--- chapter3/Excel.js
+++ chapter3/Excel.js
@@ -3,6 +3,16 @@ var Excel = React.createClass({
     getInitialState: function(){
         return {data: this.props.initialData};
     },
+    propTypes: {
+        header: React.PropTypes.arrayOf(
+            React.PropTypes.string
+        ),
+        initialData: React.PropTypes.arrayOf(
+            React.PropTypes.arrayOf(
+                React.PropTypes.string
+            )
+        ),
+    },
     render: function (){
         return (
             React.DOM.table(null,
