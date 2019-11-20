dcc4648c5c74fb31c8781a3a3ad1a433f36e8b3b
Tomo Yanagi <hideorseekyes+github@gmail.com>
2019-11-21 01:11:26 +0900

    add sort direction and enable to resort by descending

--- chapter3/Excel.js
+++ chapter3/Excel.js
@@ -1,7 +1,11 @@
 var Excel = React.createClass({
     displayName: "Excel", // displayNameプロパティを使用すると、React側に名前を伝えられる。
     getInitialState: function(){
-        return {data: this.props.initialData};
+        return {
+            data: this.props.initialData,
+            sortby: null,
+            descending: false,
+        };
     },
     propTypes: {
         header: React.PropTypes.arrayOf(
@@ -16,12 +20,16 @@ var Excel = React.createClass({
     _sort: function(e){
         var column = e.target.cellIndex;
         var data = Array.from(this.state.data)
+        var descending = this.state.sortby === column && !this.state.descending;
         data.sort((a,b)=>{
-            console.log(column);
-            return a[column] > b[column] ? 1: -1;
+            return descending
+            ? (a[column] > b[column]) ? 1 : -1
+            : (a[column] < b[column]) ? 1 : -1
         })
         this.setState({
             data: data,
+            sortby: column,
+            descending: descending,
         })
     },
     render: function (){
@@ -30,8 +38,11 @@ var Excel = React.createClass({
                 React.DOM.thead({onClick: this._sort},
                     React.DOM.tr(null,
                         this.props.headers.map((title,index)=>{
+                            if (this.state.sortby === index) {
+                                title += this.state.descending ? "↑" : "↓"
+                            }
                             return React.DOM.th({key: index}, title)
-                        })
+                        }, this)
                     )
                 ),
                 React.DOM.tbody(null,
